ea0edffead30b6d16b777f8c348e0e359e95a14e
Ihar Hrunt <Ihar.Hrunt@gmail.com>
2019-08-25 15:58:30 +0300

    Added 1.1.FabricMethod pattern

---
+++ 1.1.FabricMethod/CreatorB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1.FabricMethod
+{
+    public class CreatorB : Creator
+    {
+        public override Product FactoryMethod()
+        {            
+            return new ProductB();
+        }
+    }
+}
---
+++ 1.1.FabricMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1.FabricMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator creator;
+            Product product;
+
+            creator = new CreatorA();
+            product = creator.FactoryMethod();
+
+            creator = new CreatorB();
+            product = creator.FactoryMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
