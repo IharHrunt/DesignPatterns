32488b12435f3f903ff4387aafc6888c2ea13726
Ihar Hrunt <Ihar.Hrunt@gmail.com>
2019-09-11 23:28:59 +0300

    Added Adapter pattern

---
+++ Structural/Adapter/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter
+{
+    public class Client
+    {
+        public void Request(Target target)
+        {
+            target.Request();
+        }
+    }
+}
---
+++ Structural/Adapter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            Target adapter = new Adapter();
+            client.Request(adapter);
+
+            Console.ReadKey();
+        }
+    }
+}
